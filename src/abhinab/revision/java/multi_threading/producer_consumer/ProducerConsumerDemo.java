a8b20f22ce68e44d2d65a24c7f729e4569a06794
Abhinab Dutta Gupta <abhinab.duttagupta@gmail.com>
2025-09-14 22:57:21 +0530

    now the threads terminate and exit after completing

--- src/abhinab/revision/java/multi_threading/producer_consumer/ProducerConsumerDemo.java
+++ src/abhinab/revision/java/multi_threading/producer_consumer/ProducerConsumerDemo.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class ProducerConsumerDemo {
   public static void main(String[] args) {
-    ProducerConsumer obj = new ProducerConsumer(5);
+    ProducerConsumer obj = new ProducerConsumer(5, 4);
     Thread producer = new Thread(() -> {
       try {
         obj.produce();
@@ -12,8 +12,8 @@ public class ProducerConsumerDemo {
         System.out.println(exp.getClass().getName() + " was triggered\n\tmessage: " + exp.getMessage());
       }
     }, "producer-thread");
-    
-    Thread consumer = new Thread (new Runnable() {
+
+    Thread consumer = new Thread(new Runnable() {
       @Override
       public void run() {
         try {
@@ -26,49 +26,81 @@ public class ProducerConsumerDemo {
 
     producer.start();
     consumer.start();
+
+    while (true) {
+      if (!producer.isAlive() && !consumer.isAlive()) {
+        System.out.println("==============");
+        System.out.println("demo-completed");
+        System.out.println("==============\n");
+        break;
+      }
+    }
   }
 }
 
 class ProducerConsumer {
-  private final List<Integer> resource; //shared-resource
-  private final Object LOCK;
-  private final int limit;
-  private int counter = 0;
-  private int run;
+  private final List<Integer> resource; // shared-resource
+  private final Object LOCK; // KEY                          
+  private final int limit; // max size of the shared-resource
+  private int counter; // counting the numbers
+  private int runCounter; // how many times it was run
+  private final int runLimit; // how many times it can run
 
-  ProducerConsumer(int limit) {
+  ProducerConsumer(int limit, int runLimit) {
     LOCK = new Object();
     resource = new ArrayList<Integer>();
     this.limit = limit;
-    run = 0;
-    System.out.println("starting...");
+    this.runLimit = runLimit;
+    counter = 0;
+    runCounter = 0;
+    System.out.println("\n=====================");
+    System.out.println("initializing-the-demo");
+    System.out.println("=====================\n");
+    try {
+      Thread.sleep(1000);
+    } catch (Exception e) {
+      System.out.println(e.getClass().getName() + " was triggered\n\tmessage: " + e.getMessage());
+    }
   }
 
   public void produce() throws InterruptedException {
-    synchronized(LOCK) {
-      while (run < 6) {
+    synchronized (LOCK) {
+      while (runCounter < runLimit) {
         if (resource.size() < limit) {
           System.out.println("\tproduced: [" + (++counter) + ']');
           resource.add(counter);
+          LOCK.notify();
         } else {
           System.out.println("resource-full\n\thanding over to consumer");
+          runCounter += 1;
+          if (runCounter == runLimit) {
+            LOCK.notify();
+            break;
+          }
+          LOCK.wait();
         }
-          Thread.sleep(100);
+        Thread.sleep(300);
       }
     }
   }
 
   public void consume() throws InterruptedException {
-    synchronized(LOCK) {
-      while (run < 6) {
+    synchronized (LOCK) {
+      while (runCounter < runLimit) {
         if (!resource.isEmpty()) {
           System.out.println("\tconsumed: [" + resource.remove(0) + ']');
+          LOCK.notify();
         } else {
-          System.out.println("resource-full\n\thanding over to producer");
+          System.out.println("resource-empty\n\thanding over to producer");
+          runCounter += 1;
+          if (runCounter == runLimit) {
+            LOCK.notify();
+            break;
+          }
+          LOCK.wait();
         }
-          Thread.sleep(100);
+        Thread.sleep(300);
       }
     }
   }
 }
-
